e9f550d56ac0e34f0d83e0e333555fbdafabac5c
sajeer <sajeersayed@gmai.com>
2020-11-10 08:47:43 +0300

    inital commit

---
+++ courses/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Course
+
+class  CourseSerializer(serializers.HyperlinkedModelSerializer):#ModelSerializer
+	class Meta:
+		model = Course
+		fields = ('id', 'url','name', 'language', 'price')
+
